fc3301adbbd71ce3cee0471cdda4df9492c035ed
Edonis Seferi <doni.seferi@gmail.com>
2020-08-11 22:36:56 +0100

    err

--- src/errorHandler/errorHandler.ts
+++ src/errorHandler/errorHandler.ts
@@ -1,13 +1,13 @@
-import { badRequest, SalahResponse, error } from '../response'
+import { error as salahError, badRequest } from '../response'
 
-const errorHandler = (error: Error): SalahResponse =>
+const errorHandler = (error: Error) =>
   handlers.find((err) => err.name === error.message).response
 
 const handlers = [
   {
     name: 'DateNotISO8601Format',
     response: badRequest(
-      error(
+      salahError(
         `Please provide a date value in the ISO 8601 format. ISO 8601 Date format is yyyy-MM-dd, heres an example: api/dhuhr/date/${
           new Date(Date.now()).toISOString().split('T')[0]
         }/-0.174943`,
@@ -18,7 +18,7 @@ const handlers = [
   {
     name: 'InvalidLongitudeValue',
     response: badRequest(
-      error(
+      salahError(
         `Please provide a longitude value within a range of -180 to 180. Heres an example: api/dhuhr/date/${
           new Date(Date.now()).toISOString().split('T')[0]
         }/longitude/-0.174943`,
